0e69c45ee452077735ea29da8869264a83c0fef9
andresvz <andresvz91@gmail.com>
2015-10-14 17:29:38 -0500

    navegacion basica

--- sitio_web/views.py
+++ sitio_web/views.py
@@ -11,3 +11,28 @@ class HomeView(TemplateView):
 		context['data'] = Presentacion.objects.get(pk=1)
 		
 		return context
+
+
+class NoticiasView(TemplateView):
+	template_name = 'noticias.html'
+
+class CursosView(TemplateView):
+	template_name = 'cursos.html'
+
+class EventosView(TemplateView):
+	template_name = 'eventos.html'
+
+class LaboratoriosView(TemplateView):
+	template_name = 'laboratorios.html'
+
+class MaterialesView(TemplateView):
+	template_name = 'materiales.html'
+
+class ContactenosView(TemplateView):
+	template_name = 'contactenos.html'
+
+class GaleriaView(TemplateView):
+	template_name = 'galeria.html'
+
+class BlogView(TemplateView):
+	template_name = 'blog.html'
