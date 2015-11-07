bdf88ab00ae7cb945ed6a59240e19cea0720e504
andres <andresvz91@gmail.com>
2015-11-06 23:31:33 -0500

    modulo docente index y html base

--- docentes/views.py
+++ docentes/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, TemplateView, View
 
 class HomeView(TemplateView):
-	template_name = 'home.html'
+	template_name = 'index2.html'
