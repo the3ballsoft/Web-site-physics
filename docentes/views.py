cdbdd66317c5527d4608f18a2d5e1d87b32b584b
andres <andresvz91@gmail.com>
2015-11-09 09:46:00 -0500

    navegacion modulo docente

--- docentes/views.py
+++ docentes/views.py
@@ -3,3 +3,21 @@ from django.views.generic import ListView, DetailView, TemplateView, View
 
 class HomeView(TemplateView):
 	template_name = 'index2.html'
+
+class MaterialView(TemplateView):
+	template_name = 'vmaterial.html'
+
+class MaterialCrearView(TemplateView):
+	template_name = 'cmaterial.html'
+
+class EventoView(TemplateView):
+	template_name = 'veventos.html'
+
+class EventoCrearView(TemplateView):
+	template_name = 'ceventos.html'
+
+class NoticiasView(TemplateView):
+	template_name = 'vnoticias.html'
+
+class NoticiasCrearView(TemplateView):
+	template_name = 'cnoticias.html'
